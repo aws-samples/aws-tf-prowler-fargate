89e468b4161963321edbb9f46384f4889f16472c
raduraul <raduraul@amazon.com>
2022-08-19 11:15:38 -0700

    Updated prowler run-prowler-reports.sh to use a config file from S3

--- run-prowler-reports.sh
+++ run-prowler-reports.sh
@@ -7,8 +7,18 @@ echo "S3BUCKET:       $S3BUCKET"
 echo "S3ACCOUNT:      $S3ACCOUNT"
 echo "ROLE:           $ROLE"
 
+# Create the Prowler Output Directory if it doesn't exist
+ls ./output
+if [ $? != 0 ]; then mkdir ./output; fi
+
+# Create the Prowler Config Directory if it doesn't exist
+ls ./config
+if [ $? != 0 ]; then mkdir ./config; fi
+
 # CleanUp Last Ran Prowler Reports, as they are already stored in S3.
 rm -rf ./output/*.csv
+rm -rf ./output/*.json
+rm -rf ./output/*.html
 
 # Function to unset AWS Profile Variables
 unset_aws() {
@@ -62,10 +72,40 @@ s3_account_session() {
     export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
 }
 
+prowler_get_run_parameters() {
+    s3_account_session
+    #Obtain Prowler Config
+    aws s3 cp s3://"$S3BUCKET"/config/prowler-config.txt ./config/
+
+    # Get the scan group details from the Prowler config file
+    prowler_scan_group=$(grep -i -e "^group=.*$" ./config/prowler-config.txt | sed 's/group=//g' | xargs)
+
+    prowler_scan_group_word_count=$(echo $prowler_scan_group | wc -w)
+
+    if [ $prowler_scan_group_word_count == 0 ];then 
+        echo "[Prowler Alert]: Your config file doesn't have an scan groups listed. Defaulting to cislevel2";
+        prowler_scan_group="cislevel2";
+    fi
+
+    # Get the output formats from the Prowler config file
+    prowler_output_format=$(grep -i -e "^format=.*$" ./config/prowler-config.txt | sed 's/format=//g' | xargs | sed -e 's/[[:space:]]*//g')
+
+    prowler_output_format_word_count=$(echo $prowler_output_format | wc -w)
+
+    if [ $prowler_output_format_word_count == 0 ];then 
+        echo "[Prowler Alert]: Your config file doesn't have an output format listed. Defaulting to csv";
+        prowler_output_format="csv";
+    fi
+
+    export prowler_scan_group prowler_output_format 
+}
+
 # Run Prowler against Accounts in AWS Organization
 echo "AWS Accounts in Organization"
 echo "$ACCOUNTS_IN_ORGS"
 PARALLEL_ACCOUNTS="1"
+prowler_get_run_parameters
+
 for accountId in $ACCOUNTS_IN_ORGS; do
     # shellcheck disable=SC2015
     test "$(jobs | wc -l)" -ge $PARALLEL_ACCOUNTS && wait || true
@@ -75,23 +115,26 @@ for accountId in $ACCOUNTS_IN_ORGS; do
         unset_aws
         # Run Prowler
         echo -e "Assessing AWS Account: $accountId, using Role: $ROLE on $(date)"
+        
         # remove -g cislevel for a full report and add other formats if needed
-        ./prowler -R "$ROLE" -A "$accountId" -g cislevel2 -M csv
+        ./prowler -R "$ROLE" -A "$accountId" -g "$prowler_scan_group" -M "$prowler_output_format"
+
         echo "Report stored locally at: prowler/output/ directory"
         TOTAL_SEC=$((SECONDS - START_TIME))
         echo -e "Completed AWS Account: $accountId, using Role: $ROLE on $(date)"
         printf "Completed AWS Account: $accountId in %02dh:%02dm:%02ds" $((TOTAL_SEC / 3600)) $((TOTAL_SEC % 3600 / 60)) $((TOTAL_SEC % 60))
         echo ""
+
+        # Upload Prowler Report to S3
+        echo "Prowler Assessment Completed against $accountId. Copying report file to S3 $S3BUCKET."
+        s3_account_session
+        aws s3 mv ./output/ s3://"$S3BUCKET"/reports/ --recursive --include "*.html" --acl bucket-owner-full-control
     } &
 done
 
 # Wait for All Prowler Processes to finish
 wait
-echo "Prowler Assessments Completed against All Accounts in the AWS Organization. Starting S3 copy operations..."
 
-# Upload Prowler Report to S3
-s3_account_session
-aws s3 cp ./output/ s3://"$S3BUCKET"/reports/ --recursive --include "*.html" --acl bucket-owner-full-control
 echo "Assessment reports successfully copied to S3 bucket"
 
 # Final Wait for All Prowler Processes to finish
